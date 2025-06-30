ae1468e43c46022a4a284ef8ce3f65233911379d
sahatayz <mehmetcagataybuluc@gmail.com>
2025-06-30 15:19:14 +0300

    codes are added

---
+++ TokenPresaleContract.sol
@@ -0,0 +1,86 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.30;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+event TokensPurchased(address indexed buyer, uint256 usdcAmount, uint256 tokenAmount);
+event FundsWithdrawn(address indexed receiver, uint256 amount);
+
+contract TokenPresale is Ownable {
+    // Token being sold (your custom token)
+    IERC20 public immutable saleToken;
+
+    // Payment token (USDC)
+    IERC20 public immutable paymentToken;
+
+    // Price per token in USDC (in USDC's decimals)
+    uint256 public immutable pricePerToken;
+
+    // Maximum tokens to sell (in token's decimals)
+    uint256 public immutable saleCap;
+
+    // Track how many tokens have been sold
+    uint256 public tokensSold;
+
+    constructor(
+        address _saleToken,
+        address _paymentToken,
+        uint256 _pricePerToken,
+        uint256 _saleCap
+    ) Ownable(msg.sender) {
+        require(_saleToken != address(0), "Invalid token address");
+        require(_paymentToken != address(0), "Invalid payment token");
+        require(_pricePerToken > 0, "Price must be > 0");
+        require(_saleCap > 0, "Cap must be > 0");
+
+        saleToken = IERC20(_saleToken);
+        paymentToken = IERC20(_paymentToken);
+        pricePerToken = _pricePerToken;
+        saleCap = _saleCap;
+    }
+
+    function buy(uint256 usdcAmount) public {
+        // Input validation
+        require(usdcAmount > 0, "USDC amount must be > 0");
+
+        // Decimal conversion: (USDC-6 * TOKEN-18) / PRICE-6
+        uint256 tokenAmount = (usdcAmount * 1e18) / pricePerToken;
+
+        // Prevent fractional token amounts
+        require(tokenAmount > 0, "Token amount too small");
+
+        // Cap check
+        require(tokensSold + tokenAmount <= saleCap, "Exceeds sale cap");
+
+        // Update state
+        tokensSold += tokenAmount;
+
+        // Transfer USDC from user
+        require(
+            paymentToken.transferFrom(msg.sender, address(this), usdcAmount),
+            "USDC transfer failed"
+        );
+
+        // Transfer tokens to user
+        require(
+            saleToken.transfer(msg.sender, tokenAmount),
+            "Token transfer failed"
+        );
+
+        emit TokensPurchased(msg.sender, usdcAmount, tokenAmount);
+    }
+
+    function withdrawFunds(address to) public onlyOwner {
+        require(to != address(0), "Invalid address");
+        uint256 balance = paymentToken.balanceOf(address(this));
+        require(balance > 0, "No funds available");
+        require(paymentToken.transfer(to, balance), "Withdraw failed");
+
+        emit FundsWithdrawn(to, balance);
+    }
+
+    function remainingTokens() public view returns (uint256) {
+        return saleCap - tokensSold;
+    }
+}
